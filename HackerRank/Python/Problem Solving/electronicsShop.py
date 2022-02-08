c4a79f0ad97d30a7bd14c5ccb5ebf9a25ec492b1
Manue Ortiz Hernández <manuoh09@gmail.com>
2022-02-08 04:42:20 -0600

    Update electronicsShop.py

--- HackerRank/Python/Problem Solving/electronicsShop.py
+++ HackerRank/Python/Problem Solving/electronicsShop.py
@@ -3,17 +3,15 @@
 
 from functools import reduce
 def getMoneySpent(keyboards, drives, b):
-	sorted_a, sorted_b = list(filter(lambda e: e<b,sorted(drives,reverse=True))), list(filter(lambda e: e<b, sorted(keyboards, reverse=True)))
-	if len(keyboards) > len(drives):
-		sorted_a,sorted_b = sorted_b, sorted_a
-	prices = []
+	sorted_a = list(filter(lambda e: e<b,sorted(drives,reverse=True)))
+	sorted_b = list(filter(lambda e: e<b, sorted(keyboards, reverse=True)))
+	prices = -1
 	for f in sorted_a:
 		for s in sorted_b:
-			price = f+s
-			if price <= b:
-				prices.append(price)
-	print(prices)
-	return -1 if not prices else max(prices)
+			price = (f+s) if (f+s) <= b else -1
+			if price > prices:
+				prices = price
+	return prices
 
 if __name__ == '__main__':
 	b = 100
