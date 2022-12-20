0c870a635d13a3a833b3cc5212286e222d420025
Manuel Ortiz Hernández <manuoh09@gmail.com>
2022-12-20 14:35:42 -0600

    Keys and Room Problem

---
+++ LeetCode/Python/Medium/keysAndRoom.py
@@ -0,0 +1,20 @@
+# Manuel Ortiz at 2022
+# Extracted from: https://leetcode.com/problems/keys-and-rooms/description/
+
+
+from collections import deque
+
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        total_rooms = len(rooms)
+        queue, keys = deque(), set()
+        queue.append(0)
+        keys.add(0)
+        while queue:
+            next_room_key = queue.popleft()
+            for key in rooms[next_room_key]:
+                if key not in keys:
+                    queue.append(key)
+                    keys.add(key)
+        
+        return True if total_rooms == len(keys) else False
