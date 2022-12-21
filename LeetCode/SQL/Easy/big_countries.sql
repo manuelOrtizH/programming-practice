9b6bca05cd38eb4f588c9e94a6a29e9308ed4408
Manuel Ortiz Hernández <manuoh09@gmail.com>
2022-12-20 19:31:37 -0600

    Big Countries Query

---
+++ LeetCode/SQL/Easy/big_countries.sql
@@ -0,0 +1,7 @@
+/* Manuel Ortiz at 2022
+Extracted from: https://leetcode.com/problems/big-countries/ */
+
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000 OR population >= 25000000
+
